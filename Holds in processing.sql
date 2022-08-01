a0333f28590c870ced8475be760595a279543ae8
Kevin French <kfrench@gsinet.net>
2022-08-01 16:02:50 -0400

    modify notes

--- Holds in processing.sql
+++ Holds in processing.sql
@@ -1,3 +1,4 @@
+--Find Tool
 --Since you asked for suggestions.  This will just return the ItemRecords that could fill an Active Hold at you Library.  Change the CIR.AssignedBranchID to the one you want to search for or removed the entire line for all of them.  Run this from the Item Record Find Tool SQL.
  
 SELECT DISTINCT CIR.ItemRecordID AS RecordID
