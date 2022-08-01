846a68712c44a5452155de796cb034c25baaeaf9
Kevin French <kfrench@gsinet.net>
2022-08-01 16:06:06 -0400

    clean format

--- 3m items stuck as held hold.sql
+++ 3m items stuck as held hold.sql
@@ -1,7 +1,6 @@
+--Find Tool
 /*This issue has been around for a while now and seems to appear with titles that expire on a given date, instead of after "x" number of uses. There are hold requests on the title, so the item record status in Polaris changes to "Held." However, the item record is expired in 3M. The SQL job to set expired items to withdrawn will not modify a "Held" record, so the item gets stuck in this "Held" limbo.
-
 I've linked this ticket to an existing enhancement request for functionality to set these "Held" but expired items to "Withdrawn." In the meantime, feel free to delete these item records. If you accidentally delete an item you shouldn't, the API Consumer Service will recreate it. 
-
 I have identified all the 3M items currently stuck in held status and placed them in a record set called "3M Items stuck in held status" in case you would like to delete them. Additionally, below is a SQL script that can be ran within an Item Record Find Tool to find any more 3M records stuck in held status:*/
 
 Select ItemrecordId
