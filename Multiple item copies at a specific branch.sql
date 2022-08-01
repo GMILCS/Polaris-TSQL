11f4e2fe4cae050e6f275b9365304fc81fc95cbb
Kevin French <kfrench@gsinet.net>
2022-08-01 16:15:51 -0400

    Clean formatting

--- Multiple item copies at a specific branch.sql
+++ Multiple item copies at a specific branch.sql
@@ -1,3 +1,4 @@
+--Find Tool
 --This item record find tool SQL search is designed to find instances where a branch has more than one available book attached to a single bib record. To customize the report for your needs, you would just reblace the marker [BranchID] with your actual branch ID number from Polaris. You could also modfiy the report to change the material type searched (this query searches for books only) or the item circulation statuses searched (this query will find In, Out, Out-ILL, Held, Transferred, In-Transit, In-Repair, In-Process, Returned-ILL, and Routed items), or you might eliminate those parameters altogether. This report also excludes any items that have data in the Volume field of the item record, to exclude encyclopedia sets and other multi-volume works that use a single bib record. 
 
 SELECT IR.ItemRecordID as RecordID 
