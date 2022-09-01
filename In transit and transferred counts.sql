4fe9ba04802add843defc04d4a097a6f5956d0d3
Kevin French <kfrench@gsinet.net>
2022-09-01 15:33:09 -0400

    Update and rename SQL In transit and transferred counts.sql to In transit and transferred counts.sql

--- In transit and transferred counts.sql
+++ In transit and transferred counts.sql
@@ -2,21 +2,12 @@
 
 
 Use PolarisTransactions
-
 select org.name as SendingLibrary, orgn.name as ReceivingLibrary, count(td.transactionid) as NumberSent
-
 from polaris.transactionheaders th with(nolock) 
-
 left join polaris.transactiondetails td with(nolock) on th.Transactionid = td.transactionid
-
 left join polaris.polaris.organizations org with(nolock) on th.organizationid = org.organizationid
-
 left join polaris.polaris.organizations orgn with(nolock) on td.numValue = orgn.organizationid
-
-where th.transactiontypeid=6012 and td.transactionsubtypeid=131 and th.transactiondate > '10-1-2012'
-
-and th.transactiondate < '9-30-2013'
-
+where th.transactiontypeid=6012 and td.transactionsubtypeid=131 and th.transactiondate > '10-1-2020'
+and th.transactiondate < '9-30-2021'
 Group by org.name, orgn.name
-
 order by org.name
