bcb2c064c28b89af028d979d861ece3a198689f5
Kevin French <kfrench@gsinet.net>
2022-09-22 14:31:48 -0400

    Update Patrons Use at Circ Desk.sql

--- Patrons Use at Circ Desk.sql
+++ Patrons Use at Circ Desk.sql
@@ -1,7 +1,7 @@
 --What information do you already have?  Are you just looking for SQL or something for the Find Tool?  Here's the barest bones --kind of query, which assumes you know the workstation ID and the time frame.
 
 
-Find DISTINCT(td1.NumValue)
+Select DISTINCT(td1.NumValue)
 FROM PolarisTransactions.Polaris.TransactionHeaders AS [th] WITH (NOLOCK)
 INNER JOIN PolarisTransactions.Polaris.TransactionDetails AS [td1] WITH (NOLOCK)
 ON th.TransactionID = td1.TransactionID AND td1.TransactionSubTypeID = '6'
